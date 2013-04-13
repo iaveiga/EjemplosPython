42aa5e76dd650311667c0ee3df8fd727517ef46a
Iván Aveiga <ijaveiga@gmail.com>
2013-04-13 01:17:55 -0500

    Actualizado Engine
    Falta organizar mejor, debe haber un script con funciones básicas de
    text mining (corpus, y demás). Por ahora se limpian los tweets de RT,
    Mentions, Links, numbers, punctuacion (Cabe considerar si se debe
    eliminar hashtags).
    
    Sigo investigando sobre el limit de 200 en getTweets y como usar
    includeRT.

--- Engine.py
+++ Engine.py
@@ -1,4 +1,5 @@
 import tweepy
+import re
 
 def getApi():
 	ck = 'cAYogkcO8qrg8uarYXw'
@@ -9,12 +10,16 @@ def getApi():
 	auth.set_access_token(at,ats)
 	api = tweepy.API(auth)
 	return api
-	
-def getTweets (user_, num, includeRT):
+
+
+'''
+Obtiene los últmos n-tweets dado un usuario
+'''
+def getTweets (user_, n, includeRT):
 	user = api.get_user(user_)
 	pages = []      
 	tweets = []
-	for page in tweepy.Cursor(api.user_timeline, id = user_, count = num).pages(1):
+	for page in tweepy.Cursor(api.user_timeline, id = user_, count = n).pages(1):
 		pages.append(page)
 	
 	for page in pages:
@@ -31,4 +36,54 @@ def getMentions(user,num):
 	for page in pages:
 		for status in page:
 			mentions.apend(status)
-	return mentions	
+	return mentions
+
+'''
+Devuelve una lista con la frecuenca de los tweets
+	mañana
+	tarde
+	noche
+'''
+def freqHoraTweets(lsTweets):
+	lsFreq = [0,0,0]
+	for status in lsTweets:
+		date = str(status.created_at)
+		hour = int(date[11:13])
+		if hour >=0 and hour < 8:
+			lsFreq[0]+= 1
+		if hour >=8 and hour < 16: 
+			lsFreq[1]+= 1
+		else:
+			lsFreq[2]+= 1
+	return lsFreq
+
+
+#Devuelve el Corpus de una lista de tweets
+def corpus(lsTweets):
+        corpus = []
+        for tweet in lsTweets:
+                text = tweet.text
+                for word in text:
+                        corpus.append(word)
+        return corpus
+
+#Elimina el patrón indicado del tweet
+def cleanTweet(tweet,pattern):
+    tweet = re.sub(pattern,'',tweet)
+    return tweet
+
+#Elimina los RT, símbolos de puntuación, menciones, números, links de los tweets
+def cleanTweets(lsTweets):
+
+    pRT = "(RT|via)((?:\\b\\W*@\\w+)+)"
+    pMentions = "@\\w+"
+    pPunct = "[[:punct:]]"
+    pDig = "\d+"
+    pLinks = '(?:http://|www.)[^"\' ]+'
+    for i in range(0,len(lsTweets)):
+       lsTweets[i].text = cleanTweet(lsTweets[i].text, pRT)
+       lsTweets[i].text = cleanTweet(lsTweets[i].text, pMentions)
+       lsTweets[i].text = cleanTweet(lsTweets[i].text, pPunct)
+       lsTweets[i].text = cleanTweet(lsTweets[i].text, pDig)
+       lsTweets[i].text = cleanTweet(lsTweets[i].text, pLinks)
+    return lsTweets
